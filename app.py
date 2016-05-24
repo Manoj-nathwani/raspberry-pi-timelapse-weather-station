a9567b9d826e45145f4d30f0175531a1c1477987
Manoj Nathwani <manoj_1993@hotmail.com>
2016-05-24 23:50:04 +0100

    update

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import RPi.GPIO as GPIO
 import picamera, dht11
 import boto
 
-# init dht11
+print 'init dht11'
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 instance = dht11.DHT11(pin=14)
 
-# get dht11_data
+print 'get dht11 reading'
 dht11_data = ''
 result = instance.read()
 if result.is_valid():
@@ -20,23 +20,22 @@ if result.is_valid():
     }
     dht11_data = json.dumps(dht11_data)
 
-# take picture
+print 'take picture'
+file_name = str(time.time()).split('.')[0] + '.jpg'
 with picamera.PiCamera() as camera:
     camera.resolution = (2592, 1944)
     camera.exif_tags['EXIF.UserComment'] = dht11_data
     camera.start_preview()
     time.sleep(2)
-    camera.capture('image.jpg')
+    camera.capture(file_name)
 
-# upload image to s3
-client = boto.s3.connect_to_region('eu-west-1',
-       aws_access_key_id=os.environ["S3_KEY"],
-       aws_secret_access_key=os.environ["S3_SECRET"]
+print 'upload image to s3'
+client = boto.s3.connect_to_region(
+    'eu-west-1',
+    aws_access_key_id=os.environ["S3_KEY"],
+    aws_secret_access_key=os.environ["S3_SECRET"]
 )
-#client = boto.connect_s3(os.environ["S3_KEY"], os.environ["S3_SECRET"])
 bucket = client.get_bucket(os.environ["S3_BUCKET"])
-key = bucket.new_key('image.jpg')
-key.set_contents_from_filename('image.jpg')
+key = bucket.new_key(file_name)
+key.set_contents_from_filename(file_name)
 key.make_public()
-url = create_url(BUCKET, KEY)
-print "Uploaded image to " + url
